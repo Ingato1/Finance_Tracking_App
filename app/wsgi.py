fb8cb6e078119bbcf21cd3c01903eeb294b3c35a
Ingato1 <whitneybeatrice3@gmail.com>
2025-09-29 11:13:13 +0300

    first commit

--- app/wsgi.py
+++ app/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+
+def handler(environ, start_response):
+    """
+    Vercel serverless function handler for WSGI application.
+    """
+    return application(environ, start_response)
